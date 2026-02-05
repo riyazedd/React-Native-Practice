838fe7533a5b0b8750522f80bbd5331d9c89dfc1
riyazedd <riyaz.shrestha00@gmail.com>
2026-02-05 09:58:06 +0545

    Learned Basic Components

--- App.tsx
+++ App.tsx
@@ -1,17 +1,54 @@
-import { Alert, StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View, Image, Button, Pressable, TouchableOpacity, ScrollView, ActivityIndicator } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  
+  const onButtonPress=()=> Alert.alert("Button Pressed")
 	return (
 		<SafeAreaView style={styles.container}>
-			<Text style={styles.title}>
-				Title
+			<ScrollView
+        showsVerticalScrollIndicator={false}
+      >
+        <Text style={styles.title}>Title</Text>
+			<Text style={styles.paragraph}>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				<Text
+					style={styles.blueText}
+					onPress={() => Alert.alert("Text Pressed")}
+				>
+					Press Here!
+				</Text>
+				Facilis inventore culpa dolorum! Fugit totam quis a nulla asperiores,
+				dicta quidem labore quisquam ullam accusantium dolorem enim laborum,
+				veritatis reprehenderit ducimus?
 			</Text>
+      <ActivityIndicator size={"large"}/>
+      <View style={styles.imgContainer}>
+        <ScrollView 
+          showsHorizontalScrollIndicator={false}
+          horizontal={true}>
+        <Pressable onPress={onButtonPress}>
+          <Image source={require("./assets/icon.png")} style={styles.image1} />
+        </Pressable>
+        <TouchableOpacity onLongPress={onButtonPress}>
+          <Image source={{uri:"https://picsum.photos/200/300"}} style={styles.image2} />
+        </TouchableOpacity>
+        <Image source={{uri:"https://picsum.photos/200/300"}} style={styles.image2} />
+        </ScrollView>
+      </View>
+
       <Text style={styles.paragraph}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        <Text style={styles.blueText} onPress={()=>Alert.alert("Text Pressed")}>Press Here!</Text>
-         Facilis inventore culpa dolorum! Fugit totam quis a nulla asperiores, dicta quidem labore quisquam ullam accusantium dolorem enim laborum, veritatis reprehenderit ducimus?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis pariatur rerum autem ducimus. Sapiente accusamus perferendis voluptate aperiam aut reiciendis tempore qui exercitationem autem quam? Architecto aliquam et officiis veniam.
       </Text>
+      <Text style={styles.paragraph}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis pariatur rerum autem ducimus. Sapiente accusamus perferendis voluptate aperiam aut reiciendis tempore qui exercitationem autem quam? Architecto aliquam et officiis veniam.
+      </Text>
+      <Text style={styles.paragraph}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis pariatur rerum autem ducimus. Sapiente accusamus perferendis voluptate aperiam aut reiciendis tempore qui exercitationem autem quam? Architecto aliquam et officiis veniam.
+      </Text>
+
+      <Button title="Press Me" onPress={onButtonPress} />
+      </ScrollView>
 		</SafeAreaView>
 	);
 }
@@ -20,16 +57,29 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#fff",
+    padding:5
 		// alignItems: 'center',
 		// justifyContent: 'center',
 	},
-  title:{
-    textAlign: "center",
+	title: {
+		textAlign: "center",
 		fontSize: 50,
-  },
-  paragraph:{
-    textAlign: "justify",
+	},
+	paragraph: {
+		textAlign: "justify",
 		fontSize: 20,
+	},
+	blueText: { color: "blue", textDecorationLine: "underline" },
+  image1:{
+    width:200,
+    height:200,
   },
-  blueText:{color:"blue", textDecorationLine:"underline"}
+  image2:{
+    width:200,
+    height:200,
+    borderRadius:100
+  },
+  imgContainer:{
+    flexDirection:"row"
+  }
 });
