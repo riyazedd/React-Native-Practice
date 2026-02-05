ad2f61cea87c03087f562fe56a824aa46a616d94
riyazedd <riyaz.shrestha00@gmail.com>
2026-02-05 09:12:12 +0545

    React Native Practice

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.title}>
+				Title
+			</Text>
+      <Text style={styles.paragraph}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <Text style={styles.blueText} onPress={()=>Alert.alert("Text Pressed")}>Press Here!</Text>
+         Facilis inventore culpa dolorum! Fugit totam quis a nulla asperiores, dicta quidem labore quisquam ullam accusantium dolorem enim laborum, veritatis reprehenderit ducimus?
+      </Text>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		// alignItems: 'center',
+		// justifyContent: 'center',
+	},
+  title:{
+    textAlign: "center",
+		fontSize: 50,
   },
+  paragraph:{
+    textAlign: "justify",
+		fontSize: 20,
+  },
+  blueText:{color:"blue", textDecorationLine:"underline"}
 });
